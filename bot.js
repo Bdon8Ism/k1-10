31aa6cd26d5f56f7b24d942ccc45f7d55459e63e
Hybh <44950520+Bdon8Ism@users.noreply.github.com>
2018-12-13 12:53:43 -0800

    Add files via upload

--- bot.js
+++ bot.js
@@ -61,12 +61,12 @@ message.channel.send('#daily')
 
 bot1.on(`ready`, () => {
  
-    let x = 0; x < 90000; x++
+    for(let x = 0; x < 900; x++) {
    let channel = bot1.guilds.get("520545293096189952").channels.find(c => c.id == "520545555588317222");
    setInterval(function() {
     channel.send(`**Hello** [ " ${x} " ] `)
    }, 900);
-  
+  }
 });
 ///////////////////////////////////////
 
